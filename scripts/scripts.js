4cae9fae92199f96046146fed2eaecc8d382a3d0
Henry <henryhan110@gmail.com>
2023-03-20 13:58:29 -0700

    week11 practice

---
+++ scripts/scripts.js
@@ -0,0 +1,29 @@
+// function greet(name){
+//     console.log('Hello' + name)
+// }
+
+// greet('Henry');
+
+// let greet = function(name){
+//     console.log('Hello' + name)
+// }
+
+// greet('Henry');
+
+// let greet = (name) => {
+//     console.log('Hello' + name)
+
+// }
+// greet('Henry');
+
+// let greet = function(name){
+//     console.log('Hello' + name)
+// }
+
+// greet();
+
+let greet = function(name, time='day'){
+    console.log('Good' + time + name)
+}
+
+greet('Henry','night');
